6dd0db8062dbc1327b53cad4daec4fce3552044f
Matthew Lindfield Seager <matt@matt17r.com>
2025-01-16 16:23:46 +1100

    Layout and homepage fixes
    - tidy up unwanted tags
    - make cards slighlty dynamic
    - add mobile menu in (always visible on small screens for now)

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,11 @@
 class StaticPagesController < ApplicationController
+  Event = Struct.new(:nickname, :location, :date, :course_url)
+
   def home
+    @upcoming_events = [
+      Event.new(nickname: "Bungarribee", location: "Western Sydney Regional Park", date: "19th Jan", course_url: "#"),
+      Event.new(nickname: "Nepean River", location: "Tench Reserve, Jamesontown", date: "16th Feb", course_url: "#"),
+      Event.new(nickname: "Parramatta", location: "Parramatta Park/River", date: "16th Mar", course_url: "#")
+    ]
   end
 end
