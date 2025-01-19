44e04936fc88fe53cef6bbf84f50d54acb699d22
Matthew Lindfield Seager <matt@matt17r.com>
2025-01-20 09:36:24 +1100

    Update events list

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -9,9 +9,9 @@ class StaticPagesController < ApplicationController
 
   def home
     @upcoming_events = [
-      Event.new(nickname: "Bungarribee", location: "Bungarribee Park", date: "19th Jan", course_url: "#"),
       Event.new(nickname: "Nepean River", location: "Tench Reserve, Jamesontown", date: "16th Feb", course_url: nil),
-      Event.new(nickname: "Parramatta", location: "Parramatta Park/River", date: "16th Mar", course_url: nil)
+      Event.new(nickname: "Parramatta", location: "Parramatta Park/River", date: "16th Mar", course_url: nil),
+      Event.new(nickname: "Bungarribee", location: "Bungarribee Park", date: "20th Apr", course_url: "#"),
     ]
   end
 
