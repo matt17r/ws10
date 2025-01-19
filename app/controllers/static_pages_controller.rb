cddf3a0a3fbbfada0930c764f821447ffa83ce48
Matthew Lindfield Seager <matt@matt17r.com>
2025-01-20 09:38:51 +1100

    Fix Jamisontown spelling

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -9,7 +9,7 @@ class StaticPagesController < ApplicationController
 
   def home
     @upcoming_events = [
-      Event.new(nickname: "Nepean River", location: "Tench Reserve, Jamesontown", date: "16th Feb", course_url: nil),
+      Event.new(nickname: "Nepean River", location: "Tench Reserve, Jamisontown", date: "16th Feb", course_url: nil),
       Event.new(nickname: "Parramatta", location: "Parramatta Park/River", date: "16th Mar", course_url: nil),
       Event.new(nickname: "Bungarribee", location: "Bungarribee Park", date: "20th Apr", course_url: "#"),
     ]
