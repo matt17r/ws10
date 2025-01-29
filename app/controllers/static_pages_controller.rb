0a48336557857d30b82b7dc02126e37b174f9e00
Matthew Lindfield Seager <matt@matt17r.com>
2025-01-29 21:16:21 +1100

    Additional course information for Nepean and Parra

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,5 @@
 class StaticPagesController < ApplicationController
-  Event = Struct.new(:nickname, :location, :date, :course_url)
+  Event = Struct.new(:nickname, :location, :date)
 
   def about
   end
@@ -9,9 +9,9 @@ class StaticPagesController < ApplicationController
 
   def home
     @upcoming_events = [
-      Event.new(nickname: "Nepean River", location: "Tench Reserve, Jamisontown", date: "16th Feb", course_url: nil),
-      Event.new(nickname: "Parramatta", location: "Parramatta Park/River", date: "16th Mar", course_url: nil),
-      Event.new(nickname: "Bungarribee", location: "Bungarribee Park", date: "20th Apr", course_url: "#"),
+      Event.new(nickname: "Nepean River", location: "Tench Reserve, Jamisontown", date: "16th Feb"),
+      Event.new(nickname: "Parramatta", location: "Parramatta Park/River", date: "16th Mar"),
+      Event.new(nickname: "Bungarribee", location: "Bungarribee Park", date: "20th Apr"),
     ]
   end
 
