762d31819e7c272466a9528d9e8d30c11236087e
Matthew Lindfield Seager <matt@matt17r.com>
2025-02-16 16:29:47 +1100

    Update course and date info for March

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -11,9 +11,9 @@ class StaticPagesController < ApplicationController
 
   def home
     @upcoming_events = [
-      Event.new(nickname: "Nepean River", location: "Tench Reserve, Jamisontown", date: "16th Feb"),
       Event.new(nickname: "Parramatta", location: "Parramatta Park", date: "16th Mar"),
-      Event.new(nickname: "Bungarribee", location: "Bungarribee Park", date: "20th Apr")
+      Event.new(nickname: "Bungarribee", location: "Bungarribee Park", date: "20th Apr"),
+      Event.new(nickname: "Nepean River", location: "Tench Reserve, Jamisontown", date: "18th May")
     ]
   end
 
