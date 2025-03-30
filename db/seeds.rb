cc6b9dff06d5febfc17803862d04ace048e6fc86
Matthew Lindfield Seager <matt@matt17r.com>
2025-03-31 08:33:53 +1100

    Finish converting fixtures to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,7 @@
 Event.create(date: "2025-01-19", number: 1, location: "Bungarribee Park", description: "Cool conditions were ideal for running as a small but social group completed the inaugural WS10.")
 
-one:
-  date: "2025-01-19"
-  location: "Bungarribee Park"
-  number: 1
+Event.create(date: "2025-02-16", number: 2, location: "Nepean River", description: "Running conditions were perfect for the first WS10 at Nepean River, which saw a huge percentage increase in the number of participants 😜<br>
+Thank you all for coming along and thanks Dave for volunteering to time!")
 
-two:
-  date: "2025-02-16"
-  location: "Nepean River"
-  number: 2
-
-three:
-  date: "2025-03-16"
-  location: "Parramatta Park"
-  number: 3
+Event.create(date: "2025-03-16", number: 3, location: "Parramatta Park", description: "Unseasonably warm and humid conditions didn't prevent a record turnout for the first WS10 at Parramatta Park.<br>
+Thank you all for coming along, thanks Jacob & Rhoda for the drinks, thanks Dave for timing again and thanks Isaac for filming!")
