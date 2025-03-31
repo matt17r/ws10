5f87acd0f80dfac4f5092d1cd974ae9407baede1
Matthew Lindfield Seager <matt@matt17r.com>
2025-03-31 11:25:32 +1100

    Results route and routes tidy up

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,17 @@
 Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get "up", to: "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
   root "static_pages#home"
 
-  get "about" => "static_pages#about"
-  get "courses" => "static_pages#courses"
-  get "legacy_results" => "static_pages#results"
+  get "about", to: "static_pages#about"
+  get "courses", to: "static_pages#courses"
+  get "results", to: "static_pages#results"
+  get "new_results", to: "events#show_latest"
 
-  resource :session
+  resource :session, only: [:new, :create, :destroy]
 
-  resources :events, param: :number
-
-  get "results", to: "events#show_latest"
+  resources :events, param: :number, only: [:index, :show]
 end
