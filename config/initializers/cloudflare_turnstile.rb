9a68eca38e09aeb08bfdf322cf3edf37a969f583
Matthew Lindfield Seager <matt@matt17r.com>
2025-04-17 22:16:59 +1000

    Try to fix deployment
    Clutchiing at straws, can't figure out why this broke

--- config/initializers/cloudflare_turnstile.rb
+++ config/initializers/cloudflare_turnstile.rb
@@ -1,5 +1,5 @@
 RailsCloudflareTurnstile.configure do |c|
-  c.site_key = Rails.application.credentials.cloudflare_turnstile[:site_key]
-  c.secret_key = Rails.application.credentials.cloudflare_turnstile[:secret_key]
+  c.site_key = Rails.application.credentials.dig("cloudflare_turnstile", "site_key")
+  c.secret_key = Rails.application.credentials.dig("cloudflare_turnstile", "secret_key")
   c.fail_open = false
 end
