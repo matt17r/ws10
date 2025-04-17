8a338b6fb7d5837e633ed2bdacca988eb3b9732f
Matthew Lindfield Seager <matt@matt17r.com>
2025-04-17 22:37:29 +1000

    Fixed kamal deployment
    Finally figured it out:
     1) During assets precompile application.credentials aren't available...
     2) site_key HAS to be set for CFT Gem to be happy...
    
    Changing it to `dig` fixed 1 but site_key was still nil
    
    I don't want to use `dig` because I want it to blow up in prod if it can't access credentials...
    
    So I had to do this ugly hack to let assets precompile work
    
    Note to future self: Running the `docker buildx build --output=type=reg...` step locally let me test the docker build without having to keep commiting changes to git

--- config/initializers/cloudflare_turnstile.rb
+++ config/initializers/cloudflare_turnstile.rb
@@ -1,5 +1,10 @@
 RailsCloudflareTurnstile.configure do |c|
-  c.site_key = Rails.application.credentials.dig("cloudflare_turnstile", "site_key")
-  c.secret_key = Rails.application.credentials.dig("cloudflare_turnstile", "secret_key")
+  if ENV["SECRET_KEY_BASE_DUMMY"]
+    c.site_key = "DUMMY"
+    c.secret_key = "DUMMY"
+  else
+    c.site_key = Rails.application.credentials.cloudflare_turnstile[:site_key]
+    c.secret_key = Rails.application.credentials.cloudflare_turnstile[:secret_key]
+  end
   c.fail_open = false
 end
