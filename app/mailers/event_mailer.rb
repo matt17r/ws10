b772f07a36d9e1dc2ad10f566d97111f7639c02b
Matthew Lindfield Seager <matt@matt17r.com>
2025-05-16 23:42:37 +1000

    Include name with address (to and from)

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -1,14 +1,14 @@
 class EventMailer < ApplicationMailer
-  default from: "matt@ws10.run"
+  default from: email_address_with_name("matt@ws10.run", "Western Sydney 10")
   def result_notification(result:)
-    @event  = result.event
+    @event = result.event
     @result = result
     @user = result.user
     @result_count = @user.results.count
     @location_count = @user.results.joins(:event).where(events: { location: @event.location }).count
 
     mail(
-      to:      @user.email_address,
+      to: email_address_with_name(@user.email_address, @user.name),
       subject: "Your result for WS10 ##{@event.number} at #{@event.location}"
     )
   end
