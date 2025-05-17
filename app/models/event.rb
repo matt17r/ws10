45f119f703e16cbe1d0d1936a4a6134fd2694998
Matthew Lindfield Seager <matt@matt17r.com>
2025-05-17 23:41:24 +1000

    Send emails automatically (once event finalised)

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,8 @@ class Event < ApplicationRecord
   has_many :finish_times
   has_many :finished_users, through: :finish_positions, source: :user
 
+  after_update :send_results_emails_if_ready
+
   def unplaced_users
     User.where.not(id: finished_users.select(:id)).order(:name)
   end
@@ -24,5 +26,19 @@ class Event < ApplicationRecord
 
   def unplaced_users
     User.where.not(id: finished_users.select(:id))
+  end
+
+  private
+
+  def send_results_emails_if_ready
+    if saved_change_to_results_ready? && results_ready?
+      results.where.not(time: nil).includes(:user).find_each do |result|
+        EventMailer.result_notification(result: result).deliver_later
+      end
+
+      results.where(time: nil).includes(:user).find_each do |result|
+        EventMailer.participation_notification(result: result).deliver_later
+      end
+    end
   end
 end
