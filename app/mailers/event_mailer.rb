fc48e193d8ca0c9b09f09ad173c21296728d78dc
Matthew Lindfield Seager <matt@matt17r.com>
2025-05-17 23:40:33 +1000

    Add participation emails

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -1,5 +1,6 @@
 class EventMailer < ApplicationMailer
   default from: email_address_with_name("matt@ws10.run", "Western Sydney 10")
+
   def result_notification(result:)
     @event = result.event
     @result = result
@@ -10,6 +11,19 @@ class EventMailer < ApplicationMailer
     mail(
       to: email_address_with_name(@user.email_address, @user.name),
       subject: "Your result for WS10 ##{@event.number} at #{@event.location}"
+    )
+  end
+
+  def participation_notification(result:)
+    @event = result.event
+    @result = result
+    @user = result.user
+    @result_count = @user.results.count
+    @location_count = @user.results.joins(:event).where(events: { location: @event.location }).count
+
+    mail(
+      to: email_address_with_name(@user.email_address, @user.name),
+      subject: "You participated in WS10 ##{@event.number} at #{@event.location}"
     )
   end
 end
--- test/mailers/previews/event_mailer_preview.rb
+++ test/mailers/previews/event_mailer_preview.rb
@@ -1,8 +1,12 @@
 # Preview all emails at http://localhost:3000/rails/mailers/event_mailer
 class EventMailerPreview < ActionMailer::Preview
-  # Preview this email at http://localhost:3000/rails/mailers/event_mailer/result_notification
   def result_notification
-    result = Result.all.sample
+    result = Result.where.not(time: nil).sample
     EventMailer.result_notification(result: result)
+  end
+
+  def participation_notification
+    result = Result.where(time: nil).sample
+    EventMailer.participation_notification(result: result)
   end
 end
