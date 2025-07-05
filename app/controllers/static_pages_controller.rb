adedf89b3f0e2d7bfd612ac0d595776aebaf8f4f
Matthew Lindfield Seager <matt@matt17r.com>
2025-07-05 13:44:09 +1000

    Add upcoming events for next 3 months...
    ...and make it easier to update in future months

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -11,6 +11,15 @@ class StaticPagesController < ApplicationController
   end
 
   def courses
+    # 20th July
+    @next_bungarribee_event_fb = "https://www.facebook.com/events/1755918128619731"
+    @next_bungarribee_event_strava = "https://www.strava.com/clubs/1343589/group_events/2125976"
+    # 17th August
+    @next_nepean_event_fb = "https://www.facebook.com/events/653019341094234"
+    @next_nepean_event_strava = "https://www.strava.com/clubs/1343589/group_events/2125978"
+    # 21st September
+    @next_parramatta_event_fb = "https://www.facebook.com/events/711887074801846"
+    @next_parramatta_event_strava = "https://www.strava.com/clubs/1343589/group_events/2125983"
   end
 
   def home
